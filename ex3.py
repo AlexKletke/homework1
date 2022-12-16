df7efb109a04f8d0054c6ca5dfac158c2ac8a5c8
AlexandrKletke <klete@mail.ru>
2022-12-16 22:17:38 +0300

    added sol of the 3th ex

---
+++ ex3.py
@@ -0,0 +1,13 @@
+# Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
+# в которой находится эта точка (или на какой оси она находится).
+
+x = int(input('enter coordinate X of point, but X ≠ 0: '))
+y = int(input('enter coordinate Y of point, but Y ≠ 0: '))
+if x > 0 and y > 0:
+    print('1')
+elif x < 0 and y > 0:
+    print('2')
+elif x < 0 and y < 0:
+    print('3')
+else:
+    print('4')
